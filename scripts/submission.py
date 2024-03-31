334470da99f3e6491a15380fe659706d83b769be
Teslim Olayiwola <olayiwolateslim9@gmail.com>
2024-03-30 19:20:38 -0500

    update the fixed data path

--- scripts/submission.py
+++ scripts/submission.py
@@ -1,3 +1,4 @@
+import argparse
 from env import *
 from inference import ESP_Predictor
 import os
@@ -6,11 +7,15 @@ from tqdm import tqdm
 from utils import date_parser
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Evaluate all well(api) data.")
+    parser.add_argument("--folder", default="Train", type=str, help=" data folder path")
+    args = parser.parse_args()
+
     os.system("clear")
 
     # Load the dataframe
-    data_path = "Train"
-    esp_tracker = pd.read_excel(f"{data_path}/esp_tracker_train.xlsx",
+    esp_tracker = pd.read_excel(f"{args.folder}/esp_tracker_train.xlsx",
                                 index_col=0,
                                 date_parser=date_parser,
                                 parse_dates=["Ins Date","Startup Date","Pull date","Corrected Failure Date"])
@@ -22,7 +27,7 @@ if __name__ == "__main__":
     for api in tqdm(all_apis, total=len(all_apis), position=0):
         model = ESP_Predictor(checkpoint_path=BEST_CHECKPOINT, 
                               api=api, 
-                              csv_folder_path=data_path, 
+                              csv_folder_path=args.folder, 
                               probability=0.8)
         pred = model.predict()
 
